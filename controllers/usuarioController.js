e31a71a4f58818ebaaa445354862f27f494d1873
Nahuel Krowicki <nahuelkrowicki@gmail.com>
2020-10-21 16:21:59 -0300

    Ok backend

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,35 +1,37 @@
-const Usuario = require("../models/Usuario");
-const bcrypt = require("bcrypt");
-const { validationResult } = require("express-validator");
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 exports.nuevoUsuario = async (req, res) => {
-  
-  // Mostrar mensajes de error de express validator
-  const errores = validationResult(req);
-  if (!errores.isEmpty()) {
-    return res.status(400).json({ errores: errores.array() });
-  }
 
-  // Verificar si el usuario ya estuvo registrado
-  const { email, password } = req.body;
+    // Mostrar mensajes de error de express validator
+    const errores = validationResult(req);
+    if(!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()});
+    }
 
-  let usuario = await Usuario.findOne({ email });
+    
+    // Verificar si el usuario ya estuvo registrado
+    const { email, password } = req.body;
 
-  if (usuario) {
-    return res.status(400).json({ msg: "El usuario ya está registrado" });
-  }
+    let usuario = await Usuario.findOne({ email });
 
-  // Crear un nuevo usuario
-  usuario = new Usuario(req.body);
+    if(usuario) {
+        return res.status(400).json({ msg: 'El usuario ya esta registrado' });
+    }
 
-  // Hashear el password
-  const salt = await bcrypt.genSalt(10);
-  usuario.password = await bcrypt.hash(password, salt);
+    // Crear un nuevo usuario
+    usuario = new Usuario(req.body);
+    
+    // Hashear el password
+    const salt = await bcrypt.genSalt(10);
+    usuario.password = await bcrypt.hash(password, salt );
 
-  try {
-    await usuario.save();
-    res.json({ msg: "Usuario creado correctamente" });
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        await usuario.save();
+        res.json({msg : 'Usuario Creado Correctamente'});
+    } catch (error) {
+        console.log(error);
+    }
+
+}
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,28 @@
-const jwt = require("jsonwebtoken");
-const { validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env'});
 
-module.exports = (req, res, next) => {
-  const authHeader = req.get("Authorization");
+module.exports = (req, res, next ) => {
+    const authHeader = req.get('Authorization');
 
-  if (authHeader) {
-    // Obtener el token
-    const token = authHeader.split(" ")[1];
+    if(authHeader) {
+        // Obtener el Token 
+        const token = authHeader.split(' ')[1];
 
-    // Comprobar json web token
-    try {
-      const usuario = jwt.verify(token, process.env.SECRETA);
-      req.usuario = usuario;
-    } catch (e) {
-      console.log("JWT no valido");
-    }
-  }
-  return next();
-};
+        // console.log(token)
+
+        if(token) {
+            // comprobar el JWT
+            try {
+                const usuario = jwt.verify(token, process.env.SECRETA );
+                req.usuario = usuario;
+            } catch (error) {
+                console.log(error);
+                console.log('JWT no valido');
+            }
+        }
+
+
+    } 
+
+    return next();
+}
